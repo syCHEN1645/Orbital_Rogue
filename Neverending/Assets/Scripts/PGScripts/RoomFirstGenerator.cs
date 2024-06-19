3f31d8d749fb2e7e85266d7dbee1d3f9c12ba520
syCHEN1645 <e1121133@u.nus.edu>
2024-06-19 17:48:06 +0800

    pg
    added corridors to connect square rooms

--- Neverending/Assets/Scripts/PGScripts/RoomFirstGenerator.cs
+++ Neverending/Assets/Scripts/PGScripts/RoomFirstGenerator.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
 public class RoomFirstGenerator : SimpleRandomWalkGenerator
@@ -40,8 +37,75 @@ public class RoomFirstGenerator : SimpleRandomWalkGenerator
                 }
             }
         }
+
+        // connect approximate centres of rooms through corridors
+        List<Vector2Int> roomCentres = new List<Vector2Int>();
+        foreach (var room in roomsList) {
+            roomCentres.Add((Vector2Int)Vector3Int.RoundToInt(room.center));
+        }
+
+        HashSet<Vector2Int> corridors = ConnectRooms(roomCentres);
+        floorPositions.UnionWith(corridors);
         
         visualiser.PaintFloorTiles(floorPositions);
         WallGenerator.CreateWalls(floorPositions, visualiser);
+    }
+
+    private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCentres)
+    {
+        HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        // get a random room centre
+        var currentCentre =  roomCentres[Random.Range(0, roomCentres.Count)];
+        roomCentres.Remove(currentCentre);
+
+        while (roomCentres.Count > 0) {
+            Vector2Int nearestCentre = FindNearestCentreTo(currentCentre, roomCentres);
+            roomCentres.Remove(nearestCentre);
+            HashSet<Vector2Int> newCorridor = CreateCorridor(currentCentre, nearestCentre);
+            // set the currentCentre to nearestCentre
+            currentCentre = nearestCentre;
+            corridor.UnionWith(newCorridor);
+        }
+        return corridor;
+    }
+
+    private HashSet<Vector2Int> CreateCorridor(Vector2Int start, Vector2Int des)
+    {
+        HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        var pos = start;
+        corridor.Add(pos);
+        while (pos.y != des.y) {
+            if (des.y > pos.y) {
+                pos += Vector2Int.up;
+            } else if (des.y < pos.y) {
+                pos += Vector2Int.down;
+            }
+            corridor.Add(pos);
+        }
+        while (pos.x != des.x) {
+            if (des.x > pos.x) {
+                pos += Vector2Int.right;
+            } else if (des.x < pos.x) {
+                pos += Vector2Int.left;
+            }
+            corridor.Add(pos);
+        }
+        // here: has just reached des
+        return corridor;
+    }
+
+    private Vector2Int FindNearestCentreTo(Vector2Int currentCentre, List<Vector2Int> roomCentres)
+    {
+        Vector2Int nearestCentre = Vector2Int.zero;
+        float length = float.MaxValue;
+        foreach (var centre in roomCentres) {
+            // find smallest distance
+            float currentDistance = Vector2.Distance(centre, currentCentre);
+            if (currentDistance < length) {
+                length = currentDistance;
+                nearestCentre = centre;
+            }
+        }
+        return nearestCentre;
     }
 }
