f15e875d7790283b8d8e6b98c69b6052f65b1a51
D1ckyTwist3r <D1ckyTwist3r@users.noreply.github.com>
2024-06-30 14:45:20 +0800

    Revert "Merge new topdown to main"
    This reverts commit 8f97445f761c18337cade2a7a17f66a6bdb58d76, reversing
    changes made to ca347ce5fe2686d770dc49ed7d2f44c051dc90ac.

---
+++ Neverending/Assets/Scripts/EnemyScripts/EnemyBlueSlime.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyBlueSlime : Enemy
+{
+    protected override void InitialiseEnemy()
+    {
+        base.InitialiseEnemy();
+        speed = 0.0f;
+        attack = 20.0f;
+        attackRange = 0.0f;
+        attackInterval = 2.0f;
+    }
+    void Start()
+    {
+        InitialiseEnemy();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyHealth.IsDead()) {
+            Die();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        // if collide with player
+        if (other.gameObject.CompareTag("Player")) {
+            // deal damage to player
+            // remmber StartCorourine!!!
+            StartCoroutine(AttackPlayer());
+            // other.gameObject.TakeDamage();
+        }
+    }
+    public override void Die() {
+        base.Die();
+    }
+}
---
+++ Neverending/Assets/Scripts/PlayerScripts/PlayerInput.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public Animator animator;
+    public float groundCheckDistance = 0.1f;
+    public float wallRaycastDistance = 0.1f;
+    public ContactFilter2D groundCheckFilter;
+    private PlayerAttack playerAttack;
+    private Rigidbody2D rb;
+    private Collider2D collider2d;
+    private List<RaycastHit2D> groundHits = new List<RaycastHit2D>();
+    private List<RaycastHit2D> wallHits = new List<RaycastHit2D>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+        collider2d = GetComponent<Collider2D>();
+        playerAttack = GetComponent<PlayerAttack>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw(PAP.axisXinput);
+
+        animator.SetFloat(PAP.moveX, moveX);
+
+        bool isMoving = !Mathf.Approximately(moveX, 0f);
+
+        animator.SetBool(PAP.isMoving, isMoving);
+
+        bool lastOnGround = animator.GetBool(PAP.isOnGround);
+        bool newOnGround = CheckIfOnGround();
+        animator.SetBool(PAP.isOnGround, newOnGround);
+
+        if (lastOnGround == false && newOnGround == true) {
+            animator.SetTrigger(PAP.landedOnGround);
+        }
+
+        bool onWall = CheckIfOnWall();
+        animator.SetBool(PAP.isOnWall, onWall);
+
+        bool isJumpKeyPressed = Input.GetButtonDown(PAP.jumpKeyName);
+
+        if(isJumpKeyPressed){
+            animator.SetTrigger(PAP.JumpTriggerName);
+        } else {
+            animator.ResetTrigger(PAP.JumpTriggerName);
+        }
+
+        // attack "J"
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetBool(PAP.isAttacking, true);
+            // attack sample enemy
+            foreach(Enemy enemy in Enemy.enemyList) {
+                if (playerAttack.WithinAttackRange(enemy) && !playerAttack.IsAttacking()) {
+                    StartCoroutine(playerAttack.DealDamage(enemy.GetComponent<EnemyHealth>()));
+                }
+            }
+        }
+    }
+
+    void FixedUpdate() {
+        float forceX = animator.GetFloat(PAP.forceX);
+        
+        if (forceX != 0) rb.AddForce(new Vector2(forceX, 0) * Time.deltaTime);
+
+        float impulseX = animator.GetFloat(PAP.impulseX);
+        float impulseY = animator.GetFloat(PAP.impulseY);
+        
+        if (impulseY != 0 || impulseX != 0) {
+            float xDirectionSign = Mathf.Sign(transform.localScale.x);
+            Vector2 impulseVector = new Vector2(xDirectionSign * impulseX, impulseY);
+
+            rb.AddForce(impulseVector, ForceMode2D.Impulse);
+            animator.SetFloat(PAP.impulseY, 0);
+            animator.SetFloat(PAP.impulseX, 0);  
+        }
+
+        animator.SetFloat(PAP.velocityY, rb.velocity.y);
+
+        bool isStopVelocity = animator.GetBool(PAP.stopVelocity);
+
+        if (isStopVelocity) {
+            rb.velocity = Vector2.zero;
+            animator.SetBool(PAP.stopVelocity, false);
+        }
+    }
+
+    bool CheckIfOnGround() {
+        collider2d.Cast(Vector2.down, groundCheckFilter, groundHits, groundCheckDistance);
+
+        if(groundHits.Count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    bool CheckIfOnWall() {
+        Vector2 localScale = transform.localScale;
+
+        collider2d.Raycast(Mathf.Sign(localScale.x) * Vector2.right, groundCheckFilter, wallHits, wallRaycastDistance);
+
+        if(wallHits.Count > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void endAttack()
+    {
+        animator.SetBool(PAP.isAttacking, false);
+    }
+
+    public void Injure() {
+        // need add animation
+        // animator.SetTrigger("Hurt");
+    }
+}
