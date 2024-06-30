c4cb924c0f9b2d0f96d9f9a990c7d22a0d3a29a0
D1ckyTwist3r <D1ckyTwist3r@users.noreply.github.com>
2024-06-30 15:22:12 +0800

    Revert "Reapply "Merge new topdown to main""
    This reverts commit 053b22bde58e9c9924c73f79e8f6dc242d08d072.

--- Neverending/Assets/Scripts/Manage/InGameMenuManager.cs
+++ Neverending/Assets/Scripts/Manage/InGameMenuManager.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 
 public class InGameMenuManager : MonoBehaviour
 {
-<<<<<<< Updated upstream
     private const string MENU_SCENE = "Menu Scene";
     private GameObject pauseButton;
     private GameObject resumeButton;
@@ -17,82 +16,24 @@ public class InGameMenuManager : MonoBehaviour
         // make sure showing pause button and hiding resume button
         pauseButton.SetActive(true);
         resumeButton.SetActive(false);
-=======
-    [SerializeField]
-    private Button pauseButton;
-    [SerializeField]
-    private Button resumeButton;
-    [SerializeField]
-    private Button settingsButton;
-    [SerializeField]
-    private Button quitButton;
-    [SerializeField]
-    private GameObject settingsWindow;
-    void Start()
-    {
-        // make sure showing pause button and hiding resume button
-        pauseButton.gameObject.SetActive(true);
-        resumeButton.gameObject.SetActive(false);
-        // settings button is shown only if the game is paused
-        settingsButton.gameObject.SetActive(false);
-        settingsWindow.gameObject.SetActive(false);
->>>>>>> Stashed changes
     }
 
     // press this button to go to main menu
     public void QuitLevelButton() {
-<<<<<<< Updated upstream
         SceneManager.LoadScene(MENU_SCENE);
-=======
-        SceneManager.LoadScene(ManagerParameters.MENU_SCENE);
->>>>>>> Stashed changes
     }
 
     public void PauseGame() {
         Time.timeScale = 0;
-<<<<<<< Updated upstream
         // hide pause button, show resume button
         pauseButton.SetActive(false);
         resumeButton.SetActive(true);
-=======
-        // hide pause button, show resume button and settings button
-        pauseButton.gameObject.SetActive(false);
-        resumeButton.gameObject.SetActive(true);
-        settingsButton.gameObject.SetActive(true);
->>>>>>> Stashed changes
     }
 
     public void ResumeGame() {
         Time.timeScale = 1;
-<<<<<<< Updated upstream
         // hide resume button, show pause button
         resumeButton.SetActive(false);
         pauseButton.SetActive(true);
-=======
-        // hide resume button and settings button, show pause button
-        resumeButton.gameObject.SetActive(false);
-        settingsButton.gameObject.SetActive(false);
-        pauseButton.gameObject.SetActive(true);
-    }
-
-    public void Settings() {
-        // show settings window
-        settingsWindow.SetActive(true);
-        // when settings window is open, disable all UIs outside the window
-        pauseButton.enabled = false;
-        resumeButton.enabled = false;
-        quitButton.enabled = false;
-        settingsButton.enabled = false;
-    }
-
-    public void BackToGame() {
-        // hide settings window
-        settingsWindow.SetActive(false);
-        // enable UIs outside the window
-        pauseButton.enabled = true;
-        resumeButton.enabled = true;
-        quitButton.enabled = true;
-        settingsButton.enabled = true;
->>>>>>> Stashed changes
     }
 }
