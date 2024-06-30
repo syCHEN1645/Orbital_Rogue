246041b764712d2455f23f822aca69c13400182a
syCHEN1645 <e1121133@u.nus.edu>
2024-06-30 12:15:50 +0800

    Next level
    portal goes to transition scene instead of main menu

--- Neverending/Assets/Scripts/ManageScripts/GameManager.cs
+++ Neverending/Assets/Scripts/ManageScripts/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using UnityEngine.SceneManagement;
 using Cinemachine;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,9 +12,13 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public List<GameObject> enemies;
     public AbstractGenerator generator;
+    public VictoryPoint portal;
+    public static int level = 0;
 
     void Start()
     {
+        // increase level by 1 at the start of the level
+        level += 1;
         // generate Map, enemies, interactables and Player
         generator.GenerateMap(true);
 
@@ -28,6 +33,27 @@ public class GameManager : MonoBehaviour
         // Debug.Log(player == null);
         virtualCamera.LookAt = player.transform;
         virtualCamera.Follow = player.transform;
-        
+
+        // set portal
+        portal = GameObject.FindObjectOfType<VictoryPoint>();
+    }
+
+    void Update() {
+        // if player is at victory point, and key F is pressed
+        if (portal.atVictoryPoint && Input.GetKeyDown(KeyCode.F)) {
+            // press F to enter portal
+            if (level == ManagerParameters.MAX_LEVEL) {
+                // this level is the last level, player wins, game ends
+                PlayerWins();
+            } else {
+                // not the last level, go to transition scene and then next level
+                SceneManager.LoadScene(ManagerParameters.TRANSITION_SCENE);
+            }
+        }
+    }
+
+    private void PlayerWins()
+    {
+        throw new NotImplementedException();
     }
 }
--- Neverending/Assets/Scripts/ManageScripts/ManagerParameters.cs
+++ Neverending/Assets/Scripts/ManageScripts/ManagerParameters.cs
@@ -11,4 +11,9 @@ public static class ManagerParameters
 
     // Scene related parameters
     public const string MENU_SCENE = "Menu Scene";
+    public const string GAME_SCENE = "PG Test Scene";
+    public const string TRANSITION_SCENE = "Transition Scene";
+
+    // Level related parameters
+    public const int MAX_LEVEL = 3;
 }
