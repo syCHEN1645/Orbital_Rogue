0a0e1f119bbfb9b03d841be20aa53ffbf3af3ce9
syCHEN1645 <e1121133@u.nus.edu>
2024-07-15 12:13:55 +0800

    updated some prefab paths in pg

--- Neverending/Assets/Scripts/PG/PGPararmeters.cs
+++ Neverending/Assets/Scripts/PG/PGPararmeters.cs
@@ -7,8 +7,8 @@ public static class PGPararmeters
 {
     // list of enemies
     // may consider split into separate lists for different levels
-    public static GameObject[] typesOfEnemies = {Resources.Load("Enemy Bandit 1") as GameObject,
-        Resources.Load("Blue Slime 1") as GameObject};
+    public static GameObject[] typesOfEnemies = {Resources.Load("Prefabs/Enemy Bandit 1") as GameObject,
+        Resources.Load("Prefabs/Blue Slime 1") as GameObject};
     
     // ...
     public static List<GameObject> itemList;
@@ -16,6 +16,6 @@ public static class PGPararmeters
     public static float spawnOffsetX = 0.5f;
     public static float spawnOffsetY = 0.1f;
     // note: need to put player prefab in Resources folder
-    public static GameObject player = Resources.Load("Test Player") as GameObject;
-    public static GameObject portal = Resources.Load("Simple Portal") as GameObject;
+    public static GameObject player = Resources.Load("Prefabs/Player") as GameObject;
+    public static GameObject portal = Resources.Load("Prefabs/Simple Portal") as GameObject;
 }
