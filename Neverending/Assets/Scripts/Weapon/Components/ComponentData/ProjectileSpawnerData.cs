8b08cff98d23ce263c4a316d773c63fdf56204f6
D1ckyTwist3r <D1ckyTwist3r@users.noreply.github.com>
2024-07-19 03:11:29 +0800

    remerge player tome and magic missile

--- Neverending/Assets/Scripts/Weapon/Components/ComponentData/ProjectileSpawnerData.cs
+++ Neverending/Assets/Scripts/Weapon/Components/ComponentData/ProjectileSpawnerData.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [Serializable]
 public class ProjectileSpawnerData : ComponentData<AttackProjectileSpawner>
 {
-    [field: SerializeField] public GameObject projectilePrefab { get; private set; } 
+    [field: SerializeField] public Vector3 SpawnPositionOffset { get; private set; } 
+    [field: SerializeField] public GameObject ProjectilePrefab { get; private set; } 
 
     public ProjectileSpawnerData()
     {
